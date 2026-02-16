d4d87f1bb2e09939232a99d1a3b39b88e0c9b089
TonyStark0111 <yedhuku2002@gmail.com>
2026-02-16 20:33:23 +0530

    Update config.py
    Fix

--- config.py
+++ config.py
@@ -15,15 +15,15 @@ import os
 # ==============================
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", "15529802"))
-API_HASH = os.environ.get("API_HASH", "92bcb6aa798a6f1feadbc917fccb54d3")
+API_ID = int(os.environ.get("API_ID", "27050683"))
+API_HASH = os.environ.get("API_HASH", "013a5c0b1f2c320b98236cf212835d59")
 
 
 # ==============================
 # Admin Configuration
 # ==============================
 
-ADMINS = [int(admin) for admin in os.environ.get("ADMINS", "821215952").split(",") if admin]
+ADMINS = [int(admin) for admin in os.environ.get("ADMINS", "6048003536").split(",") if admin]
 
 
 # ==============================
