88d8361a292bd8b42cf810288e767d3063b0eac2
TonyStark0111 <yedhuku2002@gmail.com>
2026-02-16 22:41:18 +0530

    Update config.py
    Hh

--- config.py
+++ config.py
@@ -9,18 +9,18 @@ import os
 
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", "15529802"))
-API_HASH = os.environ.get("API_HASH", "92bcb6aa798a6f1feadbc917fccb54d3")
+API_ID = int(os.environ.get("API_ID", "27050683"))
+API_HASH = os.environ.get("API_HASH", "013a5c0b1f2c320b98236cf212835d59")
 
-ADMINS = [int(admin) for admin in os.environ.get("ADMINS", "821215952").split(",") if admin]
+ADMINS = [int(admin) for admin in os.environ.get("ADMINS", "6048003536").split(",") if admin]
 
 DB_URI = os.environ.get("DB_URI", "")
 DB_NAME = os.environ.get("DB_NAME", "SaveRestricted2")
 
-PUBLIC_COOLDOWN = int(os.environ.get("PUBLIC_COOLDOWN", 5))
+PUBLIC_COOLDOWN = int(os.environ.get("PUBLIC_COOLDOWN", 10))
 PRIVATE_COOLDOWN = int(os.environ.get("PRIVATE_COOLDOWN", 5))
 
-START_IMG_URL = os.environ.get("START_IMG_URL", "")  # leave empty for default
+START_IMG_URL = os.environ.get("START_IMG_URL", "https://files.catbox.moe/waqs33.jpg")  # leave empty for default
 
 AUTO_DELETE_TIME = int(os.environ.get("AUTO_DELETE_TIME", 300))  # seconds (0 = disable)
 
